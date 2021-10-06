ffb3c5c8225fac3ebc88bdefb58a5666c43248da
Akshit Bansal <akshit.bansal755@gmail.com>
2021-10-06 15:20:39 +0530

    settings are now saved to a json file

--- src/GUI/settingswidget.cpp
+++ src/GUI/settingswidget.cpp
@@ -1,25 +1,31 @@
 #include "settingswidget.h"
 #include "mainwindow.h"
-#include "src/OpenGL/camera.h"
 
 #include <QPushButton>
 #include <QHBoxLayout>
 #include <QVBoxLayout>
 #include <QLabel>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QDebug>
+#include <QFile>
 
 SettingsWidget::SettingsWidget(QWidget*parent):QWidget(parent){
     setWindowFlag(Qt::WindowStaysOnTopHint);
     setWindowTitle("Settings");
 
-    QColor defaultBGColor(0,0,0);
+    //setting default values
+    defaultBGColor=QColor(0,0,0);
+    defaultLightColor=QColor(255,255,255);
+
     bgColorDialog=new QColorDialog(defaultBGColor,this);
     bgColorDialog->setOptions(QColorDialog::NoButtons|QColorDialog::ShowAlphaChannel);
 
-    QColor defaultLightColor(255,255,255);
     lightColorDialog=new QColorDialog(defaultLightColor,this);
     lightColorDialog->setOptions(QColorDialog::NoButtons);
 
-    QColor defaultGridColor(255,255,255);
+    defaultGridColor=QColor(255,255,255);
     gridColorDialog=new QColorDialog(defaultGridColor,this);
     gridColorDialog->setOptions(QColorDialog::NoButtons);
 
@@ -43,18 +49,16 @@ SettingsWidget::SettingsWidget(QWidget*parent):QWidget(parent){
                     hb2->addWidget(btn2);
                 vb2->addLayout(hb2);
                 slider1=new SliderFloat("Ambient Light Intensity",NULL,0,100,100);
-                    float defaultAmbientLightIntensity=0.25;
                     slider1->setValue(defaultAmbientLightIntensity);
                 vb2->addWidget(slider1);
                 slider2=new SliderFloat("Diffuse Light Intensity",NULL,0,100,100);
-                        float defaultDiffuseLightIntensity=1;
                         slider2->setValue(defaultDiffuseLightIntensity);
                 vb2->addWidget(slider2);
             box2->setLayout(vb2);
         vb1->addWidget(box2);
         box3=new QGroupBox("Grid");
             box3->setCheckable(true);
-            box3->setChecked(true);
+            box3->setChecked(defaultIsShowGridChecked);
             QVBoxLayout*vb3=new QVBoxLayout;
                 QHBoxLayout*hb3=new QHBoxLayout;
                     QLabel*label3=new QLabel("Grid Color");
@@ -64,12 +68,10 @@ SettingsWidget::SettingsWidget(QWidget*parent):QWidget(parent){
                     hb3->addWidget(btn3);
                 vb3->addLayout(hb3);
                 slider3=new SliderFloat("Grid Side Length",NULL,1,1000,100);
-                    float defaultGridSideLength=5;
                     slider3->setValue(defaultGridSideLength);
                         connect(slider3,SIGNAL(valueChanged(double)),this,SLOT(gridChanged()));
                 vb3->addWidget(slider3);
                 slider4=new SliderInt("Grid Number Of Lines",NULL,2,50);
-                    int defaultGridNumLines=10;
                     slider4->setValue(defaultGridNumLines);
                         connect(slider4,SIGNAL(valueChanged(int)),this,SLOT(gridChanged()));
                 vb3->addWidget(slider4);
@@ -78,34 +80,48 @@ SettingsWidget::SettingsWidget(QWidget*parent):QWidget(parent){
         QGroupBox*box4=new QGroupBox("Camera");
             QVBoxLayout*vb4=new QVBoxLayout;
                 slider5=new SliderFloat("Camera Move Speed",NULL,1,2000,100);
-                    float defaultCameraMoveSpeed=10;
                     slider5->setValue(defaultCameraMoveSpeed);
                 vb4->addWidget(slider5);
                 slider6=new SliderFloat("Camera Turn Speed",NULL,1,10000,100000);
-                    float defaultCameraTurnSpeed=0.005;
                     slider6->setSpinBoxWidth(150);
                     slider6->setDecimals(4);
                     slider6->setValue(defaultCameraTurnSpeed);
                 vb4->addWidget(slider6);
                 slider7=new SliderFloat("Camera FOV",NULL,1,1000,320);
-                    float defaultCameraFOV=PI/4;
                     slider7->setValue(defaultCameraFOV);
                 vb4->addWidget(slider7);
                 slider8=new SliderFloat("Camera Near Z-Plane",NULL,1,10000,1000);
-                    float defaultCameraZNear=0.01;
                     slider8->setDecimals(3);
                     slider8->setSpinBoxWidth(100);
                     slider8->setValue(defaultCameraZNear);
                 vb4->addWidget(slider8);
                 slider9=new SliderFloat("Camera Far Z-Plane",NULL,1,10000,10);
-                    float defaultCameraZFar=100;
                     slider9->setDecimals(3);
                     slider9->setSpinBoxWidth(100);
                     slider9->setValue(defaultCameraZFar);
-                vb1->addWidget(slider9);
+                vb4->addWidget(slider9);
             box4->setLayout(vb4);
         vb1->addWidget(box4);
+        QHBoxLayout*hb4=new QHBoxLayout;
+            hb4->addStretch();
+            QPushButton*btn4=new QPushButton("Reset");
+                connect(btn4,SIGNAL(clicked()),this,SLOT(btn4Clicked()));
+            hb4->addWidget(btn4);
+        vb1->addLayout(hb4);
     setLayout(vb1);
+
+    loadSettings();
+}
+
+SettingsWidget::~SettingsWidget(){
+    QJsonDocument doc(settingsToJson());
+    QFile file("settings.json");
+    if(!file.open(QIODevice::WriteOnly)){
+        qDebug()<<"Unable to write settings.json";
+        return;
+    }
+    file.write(doc.toJson());
+    file.close();
 }
 
 void SettingsWidget::btn1Clicked(){
@@ -120,6 +136,22 @@ void SettingsWidget::btn3Clicked(){
     gridColorDialog->open();
 }
 
+void SettingsWidget::btn4Clicked(){
+    setBGColor(defaultBGColor);
+    setLightColor(defaultLightColor);
+    setAmbientLightIntensity(defaultAmbientLightIntensity);
+    setDiffuseLightIntensity(defaultDiffuseLightIntensity);
+    setGridColor(defaultGridColor);
+    setGridSideLength(defaultGridSideLength);
+    setGridNumLines(defaultGridNumLines);
+    setIsShowGridChecked(defaultIsShowGridChecked);
+    setCameraMoveSpeed(defaultCameraMoveSpeed);
+    setCameraTurnSpeed(defaultCameraTurnSpeed);
+    setCameraFOV(defaultCameraFOV);
+    setCameraZNear(defaultCameraZNear);
+    setCameraZFar(defaultCameraZFar);
+}
+
 bool SettingsWidget::isChangeInGrid(){
     if(changeInGrid){
         changeInGrid=false;
@@ -130,4 +162,88 @@ bool SettingsWidget::isChangeInGrid(){
 
 void SettingsWidget::gridChanged(){
     changeInGrid=true;
+}
+
+QJsonObject SettingsWidget::qColorToJson(QColor color,bool alpha){
+    QJsonObject obj;
+    obj["red"]=color.red();
+    obj["green"]=color.green();
+    obj["blue"]=color.blue();
+    if(alpha) obj["alpha"]=color.alpha();
+    return obj;
+}
+
+bool SettingsWidget::isColorJsonValid(QJsonObject json){
+    if(!json["red"].isDouble()) return false;
+    if(!json["green"].isDouble()) return false;
+    if(!json["blue"].isDouble()) return false;
+    return true;
+}
+
+QColor SettingsWidget::jsonToQColor(QJsonObject json){
+    QColor color;
+    color.setRed(json["red"].toInt());
+    color.setGreen(json["green"].toInt());
+    color.setBlue(json["blue"].toInt());
+    if(json["alpha"].isDouble()) color.setAlpha(json["alpha"].toInt());
+    else color.setAlpha(255);
+    return color;
+}
+
+QJsonObject SettingsWidget::settingsToJson(){
+    QJsonObject obj;
+        QJsonObject bgObj;
+            bgObj["backgroundColor"]=qColorToJson(getBGColor(),false);
+        obj["background"]=bgObj;
+        QJsonObject lightObj;
+            lightObj["lightColor"]=qColorToJson(getLightColor(),false);
+            lightObj["ambientLightIntensity"]=getAmbientLightIntensity();
+            lightObj["diffuseLightIntensity"]=getDiffuseLightIntensity();
+        obj["light"]=lightObj;
+        QJsonObject gridObj;
+            gridObj["showGrid"]=isShowGridChecked();
+            gridObj["gridColor"]=qColorToJson(getGridColor());
+            gridObj["gridSideLength"]=getGridSideLength();
+            gridObj["gridNumLines"]=getGridNumLines();
+        obj["grid"]=gridObj;
+        QJsonObject cameraObj;
+            cameraObj["cameraMoveSpeed"]=getCameraMoveSpeed();
+            cameraObj["cameraTurnSpeed"]=getCameraTurnSpeed();
+            cameraObj["cameraFOV"]=getCameraFOV();
+            cameraObj["cameraZNear"]=getCameraZNear();
+            cameraObj["cameraZFar"]=getCameraZFar();
+        obj["camera"]=cameraObj;
+    return obj;
+}
+
+void SettingsWidget::loadSettings(){
+    QFile file("settings.json");
+    if(!file.open(QIODevice::ReadOnly)){
+        qDebug()<<"Unable to read from settings.json";
+        return;
+    }
+    QJsonDocument doc=QJsonDocument::fromJson(file.readAll());
+    file.close();
+    if(doc.isNull()){
+        qDebug()<<"Unable to parse from settings.json";
+        return;
+    }
+    QJsonObject obj=doc.object();
+        QJsonObject bgObj=obj["background"].toObject();
+            if(isColorJsonValid(bgObj["backgroundColor"].toObject())) setBGColor(jsonToQColor(bgObj["backgroundColor"].toObject()));
+        QJsonObject lightObj=obj["light"].toObject();
+            if(isColorJsonValid(lightObj["lightColor"].toObject())) setLightColor(jsonToQColor(lightObj["lightColor"].toObject()));
+            if(lightObj["ambientLightIntensity"].isDouble()) setAmbientLightIntensity(lightObj["ambientLightIntensity"].toDouble());
+            if(lightObj["diffuseLightIntensity"].isDouble()) setDiffuseLightIntensity(lightObj["diffuseLightIntensity"].toDouble());
+        QJsonObject gridObj=obj["grid"].toObject();
+            if(gridObj["showGrid"].isBool()) setIsShowGridChecked(gridObj["showGrid"].toBool());
+            if(isColorJsonValid(gridObj["gridColor"].toObject())) setGridColor(jsonToQColor(gridObj["gridColor"].toObject()));
+            if(gridObj["gridSideLength"].isDouble()) setGridSideLength(gridObj["gridSideLength"].toDouble());
+            if(gridObj["gridNumLines"].isDouble()) setGridNumLines(gridObj["gridNumLines"].toInt());
+        QJsonObject cameraObj=obj["camera"].toObject();
+            if(cameraObj["cameraMoveSpeed"].isDouble()) setCameraMoveSpeed(cameraObj["cameraMoveSpeed"].toDouble());
+            if(cameraObj["cameraTurnSpeed"].isDouble()) setCameraTurnSpeed(cameraObj["cameraTurnSpeed"].toDouble());
+            if(cameraObj["cameraFOV"].isDouble()) setCameraFOV(cameraObj["cameraFOV"].toDouble());
+            if(cameraObj["cameraZNear"].isDouble()) setCameraZNear(cameraObj["cameraZNear"].toDouble());
+            if(cameraObj["cameraZFar"].isDouble()) setCameraZFar(cameraObj["cameraZFar"].toDouble());
 }
--- src/GUI/settingswidget.h
+++ src/GUI/settingswidget.h
@@ -4,6 +4,7 @@
 
 #include "sliderfloat.h"
 #include "sliderint.h"
+#include "src/OpenGL/camera.h"
 
 class SettingsWidget : public QWidget{
     Q_OBJECT
@@ -15,26 +16,62 @@ class SettingsWidget : public QWidget{
     QGroupBox*box3;
 
     bool changeInGrid=true;
+
+    QJsonObject qColorToJson(QColor color,bool alpha=true);
+    bool isColorJsonValid(QJsonObject json);
+    QColor jsonToQColor(QJsonObject json);
+    QJsonObject settingsToJson();
+    void loadSettings();
+
+    //default settings
+    QColor defaultBGColor;
+    QColor defaultLightColor;
+    float defaultAmbientLightIntensity=0.25;
+    float defaultDiffuseLightIntensity=1;
+    QColor defaultGridColor;
+    float defaultGridSideLength=5;
+    int defaultGridNumLines=10;
+    bool defaultIsShowGridChecked=true;
+    float defaultCameraMoveSpeed=10;
+    float defaultCameraTurnSpeed=0.005;
+    float defaultCameraFOV=PI/4;
+    float defaultCameraZNear=0.01;
+    float defaultCameraZFar=100;
 private slots:
     void btn1Clicked();
     void btn2Clicked();
     void btn3Clicked();
+    void btn4Clicked();
     void gridChanged();
 public:
     SettingsWidget(QWidget*parent=NULL);
+    ~SettingsWidget();
 
     QColor getBGColor(){return bgColorDialog->currentColor();}
+    void setBGColor(QColor color){this->bgColorDialog->setCurrentColor(color);}
     QColor getLightColor(){return lightColorDialog->currentColor();}
+    void setLightColor(QColor color){this->lightColorDialog->setCurrentColor(color);}
     float getAmbientLightIntensity(){return slider1->getValue();}
+    void setAmbientLightIntensity(float value){slider1->setValue(value);}
     float getDiffuseLightIntensity(){return slider2->getValue();}
+    void setDiffuseLightIntensity(float value){slider2->setValue(value);}
     QColor getGridColor(){return gridColorDialog->currentColor();}
+    void setGridColor(QColor color){gridColorDialog->setCurrentColor(color);}
     float getGridSideLength(){return slider3->getValue();}
+    void setGridSideLength(float value){slider3->setValue(value);}
     bool isChangeInGrid();
     int getGridNumLines(){return slider4->getValue();}
+    void setGridNumLines(int value){slider4->setValue(value);}
     bool isShowGridChecked(){return box3->isChecked();}
+    void setIsShowGridChecked(bool value){box3->setChecked(value);}
     float getCameraMoveSpeed(){return slider5->getValue();}
+    void setCameraMoveSpeed(float value){slider5->setValue(value);}
     float getCameraTurnSpeed(){return slider6->getValue();}
+    void setCameraTurnSpeed(float value){slider6->setValue(value);}
     float getCameraFOV(){return slider7->getValue();}
+    void setCameraFOV(float value){slider7->setValue(value);}
     float getCameraZNear(){return slider8->getValue();}
+    void setCameraZNear(float value){slider8->setValue(value);}
     float getCameraZFar(){return slider9->getValue();}
+    void setCameraZFar(float value){slider9->setValue(value);}
 };
